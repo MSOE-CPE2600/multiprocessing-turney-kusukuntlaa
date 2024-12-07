7ded65c16fe9a13c2128f14f3dec8e62deca93d1
Anish Kusukuntla <kusukuntlaa@msoe.edu>
2024-12-06 22:03:12 -0600

    Finished Lab12

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # System Programming Lab 11 Multiprocessing
+---
 #5
 a. The implementation of my mandelmovie.c is meant to generate a series of Mandelbrot images that are set by using multiprocessing and then combining them into an animation. The program has the getopt function to parse command-line arguments that allows the user to specify parameters such as the number of processes, frames, image dimensions, and output directory. The generate_image function would calculate the pixel colors based on the iterations of the Mandelbrot set formula such as the scaling and positioning of the set dynamically for each frame. The runtime function incorporates the semaphores and process forking to manage the generation of frames by child processes. The workload is as close to even among them to enhance performance. Each child process would generate an even amount of frames, write them as JPG files, and then exits. The processes would need to be completed, so I used a wait(NULL) at the end. 
+
 b. 
 Child Processes	  Runtime (s)
 1	               112.6
@@ -12,3 +14,15 @@ Child Processes	  Runtime (s)
 ![Screenshot](CPE2600RuntimesLab11.png)
 
 c. My results for my runtimes were from using the time ./mandelmovie -n (# of Child Processes). I used the user time to get as accurate results possible. As seen by the table and graph, I see that as the number of child processes increases, the runtime is larger since the the fork would split the frames up for each process. The rate would be by the frames/child process. An example would be if there are 10 processes and 50 frames, the runtime is larger since the amount of processes at each frame is 50/10 which is a total of 5 child processes with each one of them being at 10 frames. 
+
+# System Programming Lab 12 Multithreading
+---
+a. The updated implementation of my mandelmovie.c is meant to do some threading by using pthread. The pthread functions I used was pthread_create for creating the threads and pthread_join for joining the treads. Throughout each process, the treads would divide the task of generating an image into smaller regions. The struct called Thread_Args would store the parameters that are specific to each thread such as the region of the image to modify. The compute function would create the iteration for each pixel in the thread's assigned region. The pixel data is then stored in an image buffer. The threading would create the treads by using the compute function to create. The implementation has the processing and threading happening with the frames. I made the maximum amount of threads be 20 and the minimum being 1. If it went out of that range, then the program would just end by using exit(1).
+
+b. 
+
+![Screenshot](CPE2600Lab12Table.png)
+
+c. 
+    i. The technique that seemed to impact the runtime more was the multitreading as seen by the table. I think that the multithreading impacted the runtime more because the treads seemed to work more efficiently by splitting the tasks into different workloads. Multiprocessing seemed to be less impactful since there would be a process going to multiple of the 50 frames desired which would make less of a decrease in runtime. 
+    ii. There is a sweet spot where minimal runtime is achieved. The minimal runtime according to my table is at 16 threads and 16 processes since that is the lowest runtime. It seems that the sweet spot is when it is a maximal amount of threads. 
--- mandelmovie.c
+++ mandelmovie.c
@@ -20,13 +20,22 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <pthread.h>
-
 #define MAX 1000
 void help();
-void generate_image(char *name, double x, double y, double scale, int width, int height);
-void runtime(int num, int frame, int width, int height, char *dictionary);
-void compute_image(struct bitmap *bm, double xmin, double xmax, double ymin, double ymax, int max);
-void *compute(void *x);
+//Removed generate_image since it was being unused and it felt easier to update the
+//height and width for splitting up the regions in compute. 
+void runtime(int num, int frame, int thread, int width, int height, char *dictionary);
+void *compute(void *arg);
+//The struct is for the values for the thread to be passed into
+//creating the pthread_create
+typedef struct {
+    char name[100];
+    double x; 
+    double y; 
+    double scale;
+    int width; 
+    int height;
+} Thread_Args;
 //my main would process the command line of choices using getopt. 
 int main(int argc, char *argv[]){
     int num = 4;
@@ -36,7 +45,7 @@ int main(int argc, char *argv[]){
     char *directory = ".";
     char choice;
     int thread = 1;
-    pthread_t threading;
+    
     while((choice = getopt(argc, argv, "n:f:w:h:o:t:H")) != -1){
         switch(choice){
             case 'n':
@@ -56,72 +65,30 @@ int main(int argc, char *argv[]){
                 break;
             case 't':
                 thread = atoi(optarg);
+                if(thread < 1 || thread > 20){
+                    fprintf(stderr, "The thread value needs to be in between 1 and 20\n");
+                    exit(1);
+                }
+                break;
             case 'H':
-                help();
+                help(); 
                 exit(1);
         }
     }
-    pthread_create(&threading, NULL, &compute, &thread);
-    pthread_join(threading, NULL);
+    
     printf("frames: %d\n", frame);
     printf("Number of children: %d\n", num);
-    printf("Image size: %d x %d\n", width, height);
-    
+    printf("Initial Image size: %d x %d\n", width, height);
+    printf("treads: %d\n", thread);
     
     printf("With the amount of child processes: %d\n", num);
-    runtime(num, frame, width, height, directory);
+    runtime(num, frame, thread, width, height, directory);
     printf("Success with the frames!!\n");
     return 0;
 
 }
-//The image would be generated through using the pixels to move around 
-//on creating the image. 
-//The size of the image would be created by incorporating the width and height at
-//certain scales. 
-void generate_image(char *name, double x, double y, double scale, int width, int height){
-    imgRawImage *img = initRawImage(width, height);
-    if(!img) {
-        fprintf(stderr, "Unable to initiate image\n");
-        exit(1);
-    }
 
-    double min1 = x - scale/2;
-    double max1 = x + scale/2;
-    double min2 = y - (scale * height / width)/2;
-    double max2 = y + (scale * height / width)/2;
-    
-    for(int j = 0; j < height; j++){
-        for(int i = 0; i < width; i++){
-            double real1 = min1 + i * (max1 - min1)/width;
-            double imag1 = min2 + j * (max2 - min2)/height;
 
-            double real2 = 0;
-            double imag2 = 0;
-            int iteration = 0;
-
-            while(real2 * real2 + imag2 * imag2 <= 4 && iteration < MAX){
-                double t = real2 * real2 - imag2 * imag2 + real1;
-                imag2 = 2 * real2 * imag2 + imag1;
-                real2 = t;
-                iteration++;
-            }
-            int color;
-            if(iteration == MAX){
-                color = 0;
-            } else {
-                color = 255 - (iteration * 255/MAX);
-            }
-            setPixelRGB(img, i, j, color, color, color);
-        }
-    }
-    
-    if(storeJpegImageFile(img, name) != 0){
-        fprintf(stderr, "Unable to store %s\n", name);
-
-    }
-    freeRawImage(img);
-
-}
 //Prints out the functions you could use at the command line. 
 void help(){
     printf("Use: mandelmovie [options]\n");
@@ -131,16 +98,15 @@ void help(){
     printf("-w <int>    Width of images in pixels\n");
     printf("-h <int>    Height of images in pixels\n");
     printf("-o <dir>    The output directory for images\n");
+    printf("-t <int>    Number of threads to generate\n");
     printf("-H          Show this help text\n");
 
 }
-//would do a fork to have the amount of frames be evenly split to the processes
-//The fork would create the new process to have the system go faster. 
+//would use fork and pthreads to have the amount of frames be evenly split to the processes and threads.
+//The pthreads would create the new thread to have the system go faster. 
 //I used sem_open for the mandel_semaphore to create the images. 
 //The semaphore would close and unlink at the end. 
-void runtime(int num, int frame, int width, int height, char *dictionary){
-    
-    
+void runtime(int num, int frame, int thread, int width, int height, char *dictionary){
     sem_t *st = sem_open("/mandel_semaphore", O_CREAT | O_EXCL, 0644, num);
     if(st == SEM_FAILED){
         sem_unlink("/mandel_semaphore");
@@ -150,26 +116,60 @@ void runtime(int num, int frame, int width, int height, char *dictionary){
             exit(1);
         }
     }
+    
     double scale = (4.0)/frame;
     
     int frameschild = frame/num; 
     int remain = frame % num;
+    
     for(int i = 0; i < num; i++){
         sem_wait(st);
         pid_t pid = fork();
+        
         if(pid == 0){
             int framestart = i * frameschild;
             int frameend = framestart + frameschild;
+            
             if(i == num - 1){
                 frameend = frameend + remain;
             }
+            
+            
             for(int j = framestart; j < frameend; j++){
                 char name[100];
                 snprintf(name, 100, "%s/mandel_%d.jpg", dictionary, j);
                 double scale2 = 4.0 - j * scale;
-                generate_image(name, 0, 0, scale2, width, height);
+                pthread_t threading[thread];
+                Thread_Args args[thread];
+                
+                int r_height = height/thread;
+                for(int k = 0; k < thread; k++){
+                    snprintf(args[k].name, 100, "%s", name);
+                    args[k].x = -scale2/2;
+                    args[k].y = -scale2/2 + k * (scale2/thread);
+                    args[k].scale = scale2;
+                    args[k].width = width;
+                    if(k == thread - 1){
+                        args[k].height = height - k * r_height;
+                    } else {
+                        args[k].height = r_height;
+                    }
+
+                    if(pthread_create(&threading[k], NULL, compute, &args[k]) != 0){
+                        perror("Creating thread");
+                        exit(1);
+                    }
+                }
+                for(int k = 0; k < thread; k++) {
+                    if(pthread_join(threading[k], NULL) != 0){
+                        perror("Joining thread");
+                        exit(1);
+                    }
+                    printf("thread %d to frame %d\n", k, j);
+                }
                 
             }
+            
             sem_post(st);
             exit(0);
         } else if(pid < 0){
@@ -179,31 +179,52 @@ void runtime(int num, int frame, int width, int height, char *dictionary){
         }
         
     }
+    
     while(wait(NULL) > 0);
     sem_close(st);
     sem_unlink("/mandel_semaphore");
 }
-
-void compute_image(struct bitmap *bm, double xmin, double xmax, double ymin, double ymax, int max){
-    int i, j;
-
-    int width = bitmap_width(bm);
-    int height = bitmap_height(bm);
-
-    for(j = 0; j < height; j++){
-        for(i = 0; i < width; i++){
-            double x = xmin + i * (xmax - xmin)/width;
-            double y = ymin + j * (ymax - ymin)/height;
-
-            int iters = iterations_at_point(x, y, max);
-
-            bitmap_set(bm, i, j, iters);
-        }
+//compute gets passed in to the pthreads used in the runtime method
+//compute would update the images height and width just so that the 
+//threading would split up the region. The image would be stored once
+//the height and width are updated. 
+void *compute(void *arg){
+    Thread_Args *args = (Thread_Args *)arg;
+    imgRawImage *img = initRawImage(args->width, args->height);
+    if(!img){
+        fprintf(stderr, "Unable to initialize image\n");
+        return NULL;
     }
-}
+    
+    for(int j = 0; j < args->height; j++){
+        for(int i = 0; i < args->width; i++){
+            double real1 = args->x + i * (args->scale/args->width);
+            double imag1 = args->y + j * (args->scale/args->height);
+            double value_x = 0;
+            double value_y = 0;
+            int iteration = 0;
 
-void *compute(void *x){
-    int value = *(int*)x
-    printf("Thread is running with the value %d\n", value);
+            while(value_x * value_x + value_y * value_y <= 4 && iteration < MAX){
+                double total = value_x * value_x - value_y * value_y + real1;
+                value_y = 2 * value_x * value_y + imag1;
+                value_x = total;
+                iteration++;
+
+            }
+            int color;
+            if(iteration == MAX){
+                color = 0;
+            } else {
+                color = 255 - (iteration*255/MAX);
+                
+            }
+            setPixelRGB(img, i, j, color, color, color);
+        }
+
+    }
+    if (storeJpegImageFile(img, args->name) != 0) {
+        fprintf(stderr, "Unable to store image\n");
+    }
+    freeRawImage(img);
     return NULL;
 }
